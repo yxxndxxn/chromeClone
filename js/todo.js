e6c22c477d69b2af521783fa89f139c1929326f2
yxxndxxn <yxxndxxn@gmail.com>
2025-01-13 00:52:56 +0900

    🎨 style: 삭제 버튼 스타일 수정 및 배경 랜덤 지정

--- js/todo.js
+++ js/todo.js
@@ -2,61 +2,60 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-const TODOS_KEY ="todos";
+const TODOS_KEY = "todos";
 let toDos = [];
 
 function saveToDos() {
-    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));  //todos는 key 이름, JSON.stringify로 string으로 저장해버림
-    
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //todos는 key 이름, JSON.stringify로 string으로 저장해버림
 }
 
 function deleteToDo(event) {
-    const li = event.target.parentElement;
-    //삭제버튼을 눌렀을 때 어떤 li를 눌렀는지 확인하는 방법(= target.parentElement)
-    //target은 클릭된 HTML element(= 여기서는 button)
-    //parentElement는 클릭된 element의 부모(= 여기서는 li)
-    
-    li.remove();
-    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));  //내가 클릭한 li.id와 다른 toDo는 남겨둔다는 코드!  (클릭한 li만 삭제)
-    saveToDos();
+  const li = event.target.parentElement;
+  //삭제버튼을 눌렀을 때 어떤 li를 눌렀는지 확인하는 방법(= target.parentElement)
+  //target은 클릭된 HTML element(= 여기서는 button)
+  //parentElement는 클릭된 element의 부모(= 여기서는 li)
+
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id)); //내가 클릭한 li.id와 다른 toDo는 남겨둔다는 코드!  (클릭한 li만 삭제)
+  saveToDos();
 }
 
-function paintToDo(newTodo) {  //li, span 생성
-    const li = document.createElement("li");
-    li.id = newTodo.id;
-    const span = document.createElement("span");
-    span.innerText = newTodo.text;
-    const button = document.createElement("button");
-    button.innerHTML = "❌";
-    button.addEventListener("click", deleteToDo);
+function paintToDo(newTodo) {
+  //li, span 생성
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerHTML = "❌";
+  button.addEventListener("click", deleteToDo);
 
-    li.appendChild(span);  //li 안에 span 넣고
-    li.appendChild(button);  //li 안에 button 넣고
-    toDoList.appendChild(li);
+  li.appendChild(span); //li 안에 span 넣고
+  li.appendChild(button); //li 안에 button 넣고
+  toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
-    event.preventDefault();  //기본값 없애주기
-    const newTodo = toDoInput.value; //저장해놓고
-    toDoInput.value = "";  //비워버리기
-    const newTodoObj = {
-        text: newTodo,
-        id: Date.now(),
-    };
-    toDos.push(newTodoObj);  //toDos 배열에 newTodo 넣어버리기
-    paintToDo(newTodoObj);
-    saveToDos();
-
+  event.preventDefault(); //기본값 없애주기
+  const newTodo = toDoInput.value; //저장해놓고
+  toDoInput.value = ""; //비워버리기
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj); //toDos 배열에 newTodo 넣어버리기
+  paintToDo(newTodoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
- 
+
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
-if(savedToDos !== null){
-    const parsedToDos = JSON.parse(savedToDos);  //string을 array로 다시 저장 -> parsedToDos
-    toDos = parsedToDos;  //빈 배열이 아닌 저장했던 이전 배열들도 나타나기 위해 다시 저장
-    parsedToDos.forEach(paintToDo);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos); //string을 array로 다시 저장 -> parsedToDos
+  toDos = parsedToDos; //빈 배열이 아닌 저장했던 이전 배열들도 나타나기 위해 다시 저장
+  parsedToDos.forEach(paintToDo);
 }
 
 /*
